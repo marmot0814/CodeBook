18d897740708f09834686ebe02f4e80a31bcf5f3
Zheng-Dao Yang <yangzd0814@linux3.cs.nctu.edu.tw>
2018-04-01 12:43:17 +0800

    LeftistTree

--- DataStructure/LeftistTree.cpp
+++ DataStructure/LeftistTree.cpp
@@ -4,26 +4,16 @@ struct LeftistTree{
     struct Node{
         int v, d;
         Node *l, *r;
-        Node(int _v = 0){
-            v = _v, d = 1;
-            l = r = NULL;
-        }
-        inline int deep(){
-            return this ? d : 0;
-        }
+        Node(int _v = 0) : v(_v), d(1), l(NULL), r(NULL){}
+        inline int deep(){ return this ? d : 0; }
         inline void pull(){
-            if (l->deep() < r->deep())
-                swap(l, r);
+            if (l->deep() < r->deep()) swap(l, r);
             d = 1 + r->deep();
         }
     }*rt;
     int sz;
-    LeftistTree(){
-        sz = 0; rt = NULL;
-    }
-    ~LeftistTree(){
-        remove(rt);
-    }
+    LeftistTree(){ sz = 0; rt = NULL; }
+    ~LeftistTree(){ remove(rt); }
     Node* merge(Node *L1, Node *L2){
         if (!L1 || !L2) return L1 ? L1 : L2;
         if (L1->v < L2->v){
@@ -37,8 +27,7 @@ struct LeftistTree{
         }
     }
     void push(int v){
-        rt = merge(rt, new Node(v));
-        sz++;
+        rt = merge(rt, new Node(v)); sz++;
     }
     void pop(){
         Node *tmp = rt;
@@ -52,15 +41,9 @@ struct LeftistTree{
         sz += L->sz;
         L->sz = 0;
     }
-    int size(){ 
-        return sz; 
-    }
-    int top(){ 
-        return rt->v; 
-    }
-    bool empty(){ 
-        return !sz; 
-    }
+    int size(){  return sz; }
+    int top(){ return rt->v; }
+    bool empty(){ return !sz; }
     void remove(Node *u){ 
         if (!u) return ;
         remove(u->l);
@@ -68,39 +51,6 @@ struct LeftistTree{
         delete u; 
     }
 };
-const int MAXN = 1e5 + 5;
-LeftistTree *sol[MAXN];
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int n, m; cin >> n >> m;
-    for (int i = 0 ; i < n ; i++) sol[i] = new LeftistTree();
-    while (m--){
-        char input[20]; cin >> input;
-        int a, b;
-        switch (input[0]){
-            case 'i':
-                cin >> a >> b;
-                sol[a-1]->push(b);
-                break;
-            case 'j':
-                cin >> a >> b;
-                sol[a-1]->join(sol[b-1]);
-                break;
-            case 'e':
-                cin >> a;
-                if (sol[a-1]->empty()) cout << "NULL\n";
-                else{
-                    cout << sol[a-1]->top() << '\n';
-                    sol[a-1]->pop();
-                }
-                break;
-            case 'l':
-                cin >> a;
-                if (sol[a-1]->empty()) cout << "NULL\n";
-                else cout << sol[a-1]->top() << '\n';
-                break;
-        }
-    }
-    for (int i = 0 ; i < n ; i++) delete sol[i];
+
 }
