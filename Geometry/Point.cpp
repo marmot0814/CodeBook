df1a641a5fcb8b059d57bb45daa31cd5142c3472
Zheng-Dao Yang <yangzd0814@linux3.cs.nctu.edu.tw>
2018-04-01 13:55:36 +0800

    Point

--- Geometry/Point.cpp
+++ Geometry/Point.cpp
@@ -1,58 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-const double EPS = 1e-8;
-const double PI = acos(-1.0);
-template <typename T>
-inline char cmp(const T& a, const T& b){
-    if (abs(a - b) < EPS) return 0;
-    if (a - b < -EPS) return -1;
-    if (b - a < -EPS) return 1;
+const double EPS = 1e-10;
+int dcmp(double x){
+    if (fabs(x) < EPS) return 0;
+    return x < 0 ? -1 : 1;
 }
-template <typename T> 
 struct Point{
-    T x, y; Point(){}
-    Point(const T& x, const T& y) : x(x), y(y){}
-    Point operator + (const Point& b)const{
-        return Point(x + b.x, y + b.y);
-    }
-    Point operator - (const Point& b)const{
-        return Point(x - b.x, y - b.y);
-    }
-    Point operator * (const T& b)const{
-        return Point(x * b, y * b);
-    }
-    Point operator / (const T& b)const{
-        return Point(x / b.x, y / b.y);
-    }
-    bool operator == (const Point &b)const{
-        return !cmp(x, x.b) && !cmp(y, y.b);
-    }
-    T operator * (const Point &b)const{
-        return x * b.x + y * b.y;
-    }
-    T operator ^ (const Point &b)const{
-        return x * b.y - y * b.x;
-    }
-    Point normal()const{
-        return Point(-y, x);
-    }
-    Point len()const{
-        return (*this) * (*this);
-    }
-    T rad(const Point& b)const{
-        return fabs(atan2(fabs(cross(b)), dot(b)));
-    }
-    T getA(){
-        T A = atan2(y, x);
-        if (A <= -PI/2) A += PI * 2;
-        return A;
-    }
-    friend ostream& operator << (ostream& out, const Point& p){
-        out << "(" << p.x << "," << p.y << ")\n";
-        return out;
-    }
+    double x, y;
+    Point (double x = 0, double y = 0) : x(x), y(y){}
+    Point operator + (const Point& p) const { return Point(x + p.x, y + p.y); }
+    Point operator - (const Point& p) const { return Point(x - p.x, y - p.y); }
+    Point operator * (double p) const { return Point(x * p, y * p); }
+    Point operator / (double p) const { return Point(x / p, y / p); }
+    bool operator == (const Point& p) { return !dcmp(x - p.x) && !dcmp(y - p.y); }
+    double dot (const Point& p) const { return x * p.x + y * p.y; }
+    double len() const { return sqrt(dot(*this)); }
+    double angle (const Point& p) const { return acos(dot(p) / len() / p.len()); }
+    double cross (const Point& p) const { return x * p.y - y * p.x; }
+    double area (const Point& p1, const Point& p2) const { return (p1-*this).cross(p2-*this); }
+    Point rotate(double rad) const { return Point(x * cos(rad) - y * sin(rad), x * sin(rad) + y * cos(rad)); }
+    Point normal() const { double L = len(); return L ? Point(-y / L, x / L) : *this; }
 };
 int main(){
-    Point<int> a(1, 3), b(5, 8);
-    cout << a * b << '\n';
+
 }
