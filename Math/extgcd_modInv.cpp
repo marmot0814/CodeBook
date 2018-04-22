4caa446dba212531d30efe1b7387017c5b5d7a29
Zheng-Dao Yang <yangzd0814@linux3.cs.nctu.edu.tw>
2018-04-22 20:48:03 +0800

    extgcd_modInv

--- Math/extgcd_modInv.cpp
+++ Math/extgcd_modInv.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 typedef long long LL;
 LL extgcd(LL a, LL b, LL &x, LL &y){
-    LL d = a;
-    if (b != 0){
-        d = extgcd(b, a % b, y, x);
-        y -= (a / b) * x;
-    }else x = 1, y = 0;
-    return d;
+    if (!b) return x = 1, y = 0, a;
+    LL res = extgcd(b, a%b, y, x);
+    return y -= a / b * x, res;
 }
 LL modInv(LL a, LL m){
     LL x, y, d = extgcd(a, m, x, y);
-    return d == 1 ? (m + x % m) % m : -1;
+    return d == 1 ? (x + m) % m : -1;
 }
+
 int main(){
 
 }
